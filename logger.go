71e0a5e31bdac0c31a4f3c39638ee21eb7e95c68
Ric Allinson <capecodehq@yahoo.co.uk>
2013-08-22 22:00:14 +0800

    Updated logger with dev output

--- logger.go
+++ logger.go
@@ -3,7 +3,12 @@ package connect
 import(
     "fmt"
     "time"
+    "strconv"
 )
+
+/*
+    Options for the logger middleware.
+*/
 
 type LoggerOpt struct {
     format string
@@ -13,12 +18,51 @@ type LoggerOpt struct {
     startTime int64
 }
 
-func loggerFormat(options LoggerOpt, req *Request, res *Response) (string) {
+/*
+    Format the log for "dev" mode.
+*/
 
-    // Get the time taken in milliseconds
+func loggerFormatDev(options LoggerOpt, req *Request, res *Response) (string) {
+
+    // Get the time taken in milliseconds.
     totalTime := (time.Now().UnixNano() - options.startTime) / 1000000
 
-    return fmt.Sprint("[" + fmt.Sprint(res.StatusCode) + "] " + req.Raw.Method + " " + req.Url + " " + fmt.Sprint(totalTime) + "ms")
+    // Get the status code for the request.
+    status := res.StatusCode
+
+    // Get the length of the data sent.
+    length, _ := strconv.Atoi(res.Writer.Header().Get("Content-Length"))
+
+    // The length as a string.
+    strLen := ""
+
+    if length > 0 {
+        strLen = " - " + fmt.Sprint(strLen);
+    }
+
+    // Set the default color for the log.
+    color := 32
+
+    // Pick a color for the log.
+    switch {
+    case status >= 500:
+        color = 31
+    case status >= 400:
+        color = 33
+    case status >= 300:
+        color = 36
+    }
+
+    // Build the log line.
+    log := "\x1b[90m" + req.Raw.Method
+    log += " " + req.OriginalUrl + " "
+    log += "\x1b[" + fmt.Sprint(color) + "m" + fmt.Sprint(status)
+    log += " \x1b[90m"
+    log += fmt.Sprint(totalTime)
+    log += "ms" + strLen
+    log += "\x1b[0m"
+
+    return log
 }
 
 func Logger(options LoggerOpt) (func(req *Request, res *Response, next func())) {
@@ -34,11 +78,11 @@ func Logger(options LoggerOpt) (func(req *Request, res *Response, next func())) 
 
         // Decide if we should log immediately or at the end of the request.
         if immediate {
-            line := loggerFormat(options, req, res)
+            line := loggerFormatDev(options, req, res)
             fmt.Println(line)
         } else {
             next()
-            line := loggerFormat(options, req, res)
+            line := loggerFormatDev(options, req, res)
             fmt.Println(line)
         }
     }
