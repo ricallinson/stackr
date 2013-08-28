e849f9426f88de6c0dca8fae6326522e76c60f7f
Ric Allinson <capecodehq@yahoo.co.uk>
2013-08-28 10:52:38 -0700

    Added basic benchmark for testing

---
+++ stackr_benchmark_test.go
@@ -0,0 +1,75 @@
+package stackr
+
+import(
+    // "fmt"
+    "testing"
+)
+
+func BenchmarkMatchOneHandle(b *testing.B) {
+
+    req := createRequest(NewMockHttpRequest())
+    res := createResponse(NewMockResponseWriter(false))
+
+    app := CreateServer()
+    app.Use("/", func(req *Request, res *Response, next func()) {})
+
+    b.ResetTimer()
+
+    for i := 0; i < b.N; i++ {
+        app.handle(req, res, 0)
+    }
+}
+
+func BenchmarkMatchTenHandles(b *testing.B) {
+
+    req := createRequest(NewMockHttpRequest())
+    res := createResponse(NewMockResponseWriter(false))
+
+    app := CreateServer()
+
+    for i := 0; i < 10; i++ {
+        app.Use("/", func(req *Request, res *Response, next func()) {})
+    }
+
+    b.ResetTimer()
+
+    for i := 0; i < b.N; i++ {
+        app.handle(req, res, 0)
+    }
+}
+
+func BenchmarkMatchFiftyHandles(b *testing.B) {
+
+    req := createRequest(NewMockHttpRequest())
+    res := createResponse(NewMockResponseWriter(false))
+
+    app := CreateServer()
+
+    for i := 0; i < 50; i++ {
+        app.Use("/", func(req *Request, res *Response, next func()) {})
+    }
+
+    b.ResetTimer()
+
+    for i := 0; i < b.N; i++ {
+        app.handle(req, res, 0)
+    }
+}
+
+func BenchmarkMatchThousandHandles(b *testing.B) {
+
+    req := createRequest(NewMockHttpRequest())
+    res := createResponse(NewMockResponseWriter(false))
+
+    app := CreateServer()
+
+    for i := 0; i < 1000; i++ {
+        app.Use("/", func(req *Request, res *Response, next func()) {})
+    }
+
+    b.ResetTimer()
+
+    for i := 0; i < b.N; i++ {
+        app.handle(req, res, 0)
+    }
+}
