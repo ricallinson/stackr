0e5ac8faa580ca0f4b64dfc51fcf17972cbaeb0b
Ric Allinson <capecodehq@yahoo.co.uk>
2013-11-19 21:39:23 +0800

    Removed time.Sleep() test code

--- response_time.go
+++ response_time.go
@@ -19,7 +19,6 @@ func ResponseTime() func(req *Request, res *Response, next func()) {
 	return func(req *Request, res *Response, next func()) {
 		start := time.Now().UnixNano()
 		res.On("header", func() {
-			time.Sleep(100 * time.Millisecond)
 			duration := time.Now().UnixNano() - start
 			res.SetHeader("X-Response-Time", strconv.FormatInt(int64(time.Duration(duration)/time.Millisecond), 10)+"ms")
 		})
